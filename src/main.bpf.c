7f723fb4fdddfc102efbb16e6b7c6b18a3525bec
Sergey Andreev <andrejew.sw@gmail.com>
2024-04-23 23:49:11 +0300

    Updated the license of the bpf module

--- src/main.bpf.c
+++ src/main.bpf.c
@@ -8,4 +8,4 @@ int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter *ctx
   return 0;
 }
 
-const char LICENSE[] SEC("license") = "GPL";
+const char LICENSE[] SEC("license") = "Dual MIT/GPL";
